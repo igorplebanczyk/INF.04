37480a5e595779925fe7924aef5ad6d86054a061
igorplebanczyk <plebanczyk.igor@gmail.com>
2026-04-29 18:41:04 +0200

    add INF.04-01-23.01-SG

---
+++ egzaminy/INF.04-01-23.01-SG/desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System.Windows;
+
+namespace _2023_styczen_01_desktopowa
+{
+    public partial class MainWindow : Window
+    {
+        string password;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePasswordButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (charactersCountTextBox == null || uppercaseLettersCheckbox == null || integersCheckbox == null || specialCharactersCheckbox == null) return;
+
+            int charactersCount = int.Parse(charactersCountTextBox.Text);
+            bool useUppercaseLetters = (bool)uppercaseLettersCheckbox.IsChecked;
+            bool useIntegers = (bool)integersCheckbox.IsChecked;
+            bool useSpecialCharacters = (bool)specialCharactersCheckbox.IsChecked;
+
+            const string lowercaseLetters = "aąbcćdeęfghijklmnńoópqrsśtuvwxyzźż";
+            const string uppercaseLetters = "AĄBCĆDEĘFGHIJKLMNŃOÓPQRSŚTUVWXYZ";
+            const string specialCharacters = "!@#$%^&*()_+-=";
+
+            string password = "";
+            Random random = new();
+
+            if (useUppercaseLetters)
+            {
+                password += uppercaseLetters[random.Next(0, uppercaseLetters.Length)];
+                charactersCount--;
+            }
+
+            if (useIntegers)
+            {
+                password += random.Next(0, 10).ToString();
+                charactersCount--;
+            }
+
+            if (useSpecialCharacters)
+            {
+                password += specialCharacters[random.Next(0, specialCharacters.Length)];
+                charactersCount--;
+            }
+
+            for (int i = 0; i < charactersCount; i++)
+            {
+                password += lowercaseLetters[random.Next(0, lowercaseLetters.Length)];
+            }
+
+            this.password = password;
+            MessageBox.Show(this.password);
+        }
+
+        private void ConfirmButtonClick(object sender, RoutedEventArgs e)
+        {
+            string firstName = firstNameTextBox.Text;
+            string lastName = lastNameTextBox.Text;
+            string position = positionComboBox.SelectionBoxItem.ToString();
+
+            MessageBox.Show("Dane pracownika " + firstName + " " + lastName + " " + position + " Hasło: " + this.password);
+        }
+    }
+}
---
+++ egzaminy/INF.04-01-23.01-SG/konsola/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Wprowadź liczbę całkowitą a: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Wprowadź liczbę całkowitą b: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Największy wspólny dzielnik liczb a i b wynosi " + nwd(a, b));
+
+/**********************************************
+nazwa funkcji: nwd
+opis funkcji: oblicza największy wspólny dzielnik dwóch liczb całkowitych
+parametry: a - pierwsza z pary liczb, dla której obliczane będzie nwd
+b - drugie z pary liczb, dla której obliczane będzie nwd
+zwracany typ i opis: int - zwracany jest największy wspólny dzielnik liczb a i b
+autor: 123456789012
+***********************************************/
+int nwd(int a, int b)
+{
+    while (a != b)
+    {
+        if (a > b) a -= b;
+        else b -= a;
+    }
+
+    return a;
+}
