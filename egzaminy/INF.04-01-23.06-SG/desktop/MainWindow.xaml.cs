f4a222ea26f677296ee427c6cdd952146bc27ffb
igorplebanczyk <plebanczyk.igor@gmail.com>
2026-04-30 19:24:08 +0200

    add INF.04-01-23.06-SG

---
+++ egzaminy/INF.04-01-23.06-SG/desktop/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2023_czerwiec_01_desktopowa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void CheckPriceButton(object sender, RoutedEventArgs e)
+        {
+            if (RadioButtonPostcard.IsChecked == true)
+            {
+                LabelPrice.Content = "Cena: 1 zł";
+                ImageType.Source = new BitmapImage(new Uri("/images/pocztowka.png", UriKind.Relative));
+            }
+
+            if (RadioButtonLetter.IsChecked == true)
+            {
+                LabelPrice.Content = "Cena: 1,5 zł";
+                ImageType.Source = new BitmapImage(new Uri("/images/list.png", UriKind.Relative));
+            }
+
+            if (RadioButtonPackage.IsChecked == true)
+            {
+                LabelPrice.Content = "Cena: 10 zł";
+                ImageType.Source = new BitmapImage(new Uri("/images/paczka.png", UriKind.Relative));
+            }
+        }
+
+        public void ConfirmButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxPostalCode.Text.Length != 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+                return;
+            }
+
+            if (!int.TryParse(TextBoxPostalCode.Text, out int postalCode))
+            {
+                MessageBox.Show("Kod pocztowy powinien składać się z samych cyfr");
+                return;
+            }
+
+            MessageBox.Show("Dane przesyłki zostały wprowadzone");
+        }
+    }
+}
---
+++ egzaminy/INF.04-01-23.06-SG/konsola/Program.cs
@@ -0,0 +1,48 @@
+﻿/*******************************************************
+nazwa funkcji: wypelnijTablice
+parametry wejściowe: tablica - tablice do wypelnienia
+wartość zwracana: brak
+informacje: funkcja wypelnia tablice typu logicznego wartościami true
+autor: 12345678901
+****************************************************/
+void wypelnijTablice(bool[] tablica)
+{
+    for (int i = 2; i < tablica.Length; i++)
+    {
+        tablica[i] = true;
+    }
+}
+
+List<int> sitoErastotenesa(bool[] tablica)
+{
+    List<int> indeksyWartosciTrue = [];
+
+    for (int i = 2; i <= Math.Sqrt(tablica.Length - 1); i++)
+    {
+        if (tablica[i])
+        {
+            for (int j = 2 * i; j < tablica.Length; j += i)
+            {
+                tablica[j] = false;
+            }
+        }
+    }
+
+    for (int i = 2; i < tablica.Length; i++)
+    {
+        if (tablica[i])
+        {
+            indeksyWartosciTrue.Add(i);
+        }
+    }
+
+    return indeksyWartosciTrue;
+}
+
+bool[] tablica = new bool[101];
+wypelnijTablice(tablica);
+
+List<int> liczbyPierwsze = sitoErastotenesa(tablica);
+
+Console.WriteLine("Liczby pierwsze w przedziale 2 do 100: ");
+foreach (int num in liczbyPierwsze) Console.Write(num + " ");
