71e2330c2e2c21f75af8779df69a48a1bc35348e
igorplebanczyk <plebanczyk.igor@gmail.com>
2026-05-05 09:46:11 +0200

    INF.04-01-24.01-SG

---
+++ egzaminy/INF.04-01-24.01-SG/desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace _2024_styczen_01_desktopowa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NumberTextBoxLostFocus(object sender, RoutedEventArgs e)
+        {
+            string number = TextBoxNumber.Text;
+
+            ImageFingerprint.Source = new BitmapImage(new Uri("/images/" + number + "-odcisk.jpg", UriKind.Relative));
+            ImagePerson.Source = new BitmapImage(new Uri("/images/" + number + "-zdjecie.jpg", UriKind.Relative));
+        }
+
+        private void OkButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (TextBoxName.Text == "" || TextBoxLastName.Text == "")
+            {
+                MessageBox.Show("Wprowadź dane");
+                return;
+            }
+
+            string eyeColor = "";
+
+            if (RadioButtonBlue.IsChecked == true) eyeColor = "niebieskie";
+            if (RadioButtonGreen.IsChecked == true) eyeColor = "zielone";
+            if (RadioButtonBrown.IsChecked == true) eyeColor = "piwne";
+
+
+            MessageBox.Show(TextBoxName.Text + " " + TextBoxLastName.Text + " kolor oczu " + eyeColor);
+        }
+    }
+}
---
+++ egzaminy/INF.04-01-24.01-SG/konsola/Program.cs
@@ -0,0 +1,37 @@
+﻿char getSex(int[] pesel)
+{
+    if (pesel[9] % 2 == 0) return 'K';
+    else return 'M';
+}
+
+/**********************************************
+nazwa funkcji: isValid
+opis funkcji: sprawdza czy numer PESEL jest poprawny na podstawie sumy kontrolnej
+parametry: pesel - numer pesel w postaci tablicy liczb całkowitych
+zwracany typ i opis: bool - true albo false w zależności od zgodności sumy kontrolnej
+autor: 12345678901
+***********************************************/
+bool isValid(int[] pesel)
+{
+    int s = pesel[0] + pesel[1] * 3 + pesel[2] * 7 + pesel[3] * 9 + pesel[4] + pesel[5] * 3 + pesel[6] * 7 + pesel[7] * 9 + pesel[8] + pesel[9] * 3;
+    int m = s % 10;
+    int r;
+
+    if (m == 0) r = 0;
+    else r = 10 - m;
+
+    return pesel[10] == r;
+}
+
+Console.WriteLine("Podaj numer PESEL: ");
+int[] pesel = [5, 5, 0, 3, 0, 1, 0, 1, 1, 9, 3];
+for (int i = 0; i < pesel.Length; i++)
+{
+    pesel[i] = int.Parse(Console.Read().ToString()) - '0';
+}
+Console.WriteLine("");
+
+if (getSex(pesel) == 'K') Console.WriteLine("Kobieta");
+else Console.WriteLine("Mężczyzna");
+
+Console.WriteLine(isValid(pesel));
