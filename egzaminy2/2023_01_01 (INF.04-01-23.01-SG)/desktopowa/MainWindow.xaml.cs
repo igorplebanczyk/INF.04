d25fbd828ff993d3ffa6a1afc79ab13c5b556b0d
igorplebanczyk <plebanczyk.igor@gmail.com>
2026-05-19 09:59:34 +0200

    add INF.04-01-23.01-SG (2)

---
+++ egzaminy2/2023_01_01 (INF.04-01-23.01-SG)/desktopowa/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace _2023_01_01_d
+{
+    public partial class MainWindow : Window
+    {
+        string password = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneratePassword(object sender, RoutedEventArgs e)
+        {
+            string lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+            string uppercaseLetters = lowercaseLetters.ToUpper();
+            string integers = "0123456789";
+            string specialCharacters = "@#$%^&*()_+-=";
+
+            string characterSet = lowercaseLetters;
+            string password = "";
+            int charactersCount = int.Parse(textBoxCharactersCount.Text);
+
+            Random random = new();
+
+            if (checkBoxUppercaseLetters.IsChecked == true) characterSet += uppercaseLetters;
+            if (checkBoxIntegers.IsChecked == true) characterSet += integers;
+            if (checkBoxSpecialCharacters.IsChecked == true) characterSet += specialCharacters;
+
+            for (int i = 0; i < charactersCount; i++) password += characterSet[random.Next(0, characterSet.Length)];
+
+            this.password = password;
+            MessageBox.Show(password);
+        }
+
+        private void Confirm(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Dane pracownika: " + textBoxName.Text + " " + textBoxLastName.Text + " " + comboBoxPosition.Text + " Hasło: " + password);
+        }
+    }
+}
---
+++ egzaminy2/2023_01_01 (INF.04-01-23.01-SG)/konsola/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj pierwszą liczbę:");
+        uint a = uint.Parse(Console.ReadLine());
+
+        Console.WriteLine("Podaj drugą liczbę:");
+        uint b = uint.Parse(Console.ReadLine());
+
+        Console.WriteLine("NWD liczb " + a + " i " + b + " wynosi " + Nwd(a, b));
+    }
+
+    /**********************************************
+    nazwa funkcji: Nwd
+    opis funkcji: oblicza NWD dwóch podanych liczb
+    parametry: a - pierwsza liczba z pary, dla której będzie obliczane NWD
+    b - druga liczba z pary, dla której będzie obliczane NWD
+    zwracany typ i opis: uint - NWD dwóch podanych liczb
+    autor: 01234567890
+    ***********************************************/
+    static uint Nwd(uint a, uint b) 
+    { 
+        while (a != b)
+        {
+            if (a > b) a -= b;
+            else b -= a;
+        }
+
+        return a;
+    }
+}
