863227e58b9d0179090e3a1a1292facdfdb9095a
igorplebanczyk <plebanczyk.igor@gmail.com>
2026-05-21 10:39:13 +0200

    add INF.04-01-23.06-SG (2)

---
+++ egzaminy2/2023_06_01 (INF.04-01-23.06-SG)/desktop/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace _2023_06_01_d
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckPrice(object sender, RoutedEventArgs e)
+        {
+            if (radioButtonPostcard.IsChecked == true)
+            {
+                imagePackageType.Source = new BitmapImage(new Uri("/images/pocztowka.png", UriKind.Relative));
+                labelPrice.Content = "Cena: 1zł";
+            }
+
+            if (radioButtonLetter.IsChecked == true)
+            {
+                imagePackageType.Source = new BitmapImage(new Uri("/images/list.png", UriKind.Relative));
+                labelPrice.Content = "Cena: 1,5zł";
+            }
+
+            if (radioButtonPackage.IsChecked == true)
+            {
+                imagePackageType.Source = new BitmapImage(new Uri("/images/paczka.png", UriKind.Relative));
+                labelPrice.Content = "Cena: 10zł";
+            }
+        }
+
+        private void Confirm(object sender, RoutedEventArgs e)
+        {
+            if (textBoxPostalCode.Text.Length != 5)
+            {
+                MessageBox.Show("Nieprawidłowa liczba cyfr w kodzie pocztowym");
+                return;
+            }
+
+            if (!int.TryParse(textBoxPostalCode.Text, out _))
+            {
+                MessageBox.Show("Kod pocztowy powinien się składać się z samych cyfr");
+                return;
+            }
+
+            MessageBox.Show("Dane przesyłki zostały wprowadzone");
+        }
+    }
+}
---
+++ egzaminy2/2023_06_01 (INF.04-01-23.06-SG)/konsola/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+    static void Main()
+    {
+        bool[] array = new bool[101];
+        FillArray(array);
+        FindPrimes(array);
+
+        Console.Write("Liczby pierwsze: ");
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == true) Console.Write(i + " ");
+        }
+    }
+
+    /*******************************************************
+    nazwa funkcji: FillArray
+    parametry wejściowe: array - tablicę do wypełnienia
+    wartość zwracana: brak
+    informacje: funkcja wypełnia daną tablicę typu bool wartościami true, zaczynając od drugiego indeksu
+    autor: 01234567890
+    ****************************************************/
+    static void FillArray(bool[] array)
+    {
+        for (int i = 2; i < array.Length; i++) array[i] = true;
+    }
+
+    static void FindPrimes(bool[] array)
+    {
+        for (int i = 2; i <= Math.Sqrt(array.Length - 1); i++)
+        {
+            if (array[i] == true)
+            {
+                for (int j = 2 * i; j <= array.Length - 1; j += i)
+                {
+                    array[j] = false;
+                }
+            }
+        }
+    }
+}
